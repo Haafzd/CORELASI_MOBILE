91a3e835950176c09c5386dbe51d8874d1bb9a4c
hafidzmusyafa <hafidzmusyafa@student.telkomuniversity.ac.id>
2025-11-18 16:03:11 +0700

    yang bener?

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,3 +1,4 @@
+import 'package:corelasi_simple/screens/bap_page.dart';
 import 'package:flutter/material.dart';
 import '../screens/setting_page.dart';
 import '../screens/attendance.dart';
@@ -7,10 +8,12 @@ class AppRoutes {
   static const String editAccount = '/edit_account';
   static const String attendance = '/attendance';
   static const String home = '/home';
+  static const String bap = '/bap';
 
   static Map<String, WidgetBuilder> routes = {
     editAccount: (context) => const SettingPage(),
     attendance: (context) => const Attendance(),
     home: (context) => const HomePage(),
+    bap: (context) => const BapPage(),
   };
 }
