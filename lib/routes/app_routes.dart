58a585b9d1e89574a2ec9006894aeee1bc55e226
billafza <nashbillanurfazza@gmail.com>
2025-11-18 16:06:31 +0700

    save locall

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -14,6 +14,6 @@ class AppRoutes {
     editAccount: (context) => const SettingPage(),
     attendance: (context) => const Attendance(),
     home: (context) => const HomePage(),
-    notification: (context) => const NotificationsPage(),
+    notification: (context) => const NotificationScreen(),
   };
 }
