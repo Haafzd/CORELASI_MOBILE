2a093b206b760307949ecae6cdb13d6f4a770c20
gilang <156637129+gilang1223@users.noreply.github.com>
2026-01-07 11:38:06 +0700

    Add profile page UI

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import '../screens/setting_page.dart';
 import '../screens/attendance.dart';
 import '../screens/home_page.dart';
 import '../screens/notification_screen.dart';
+import '../screens/profile_page.dart';
 
 class AppRoutes {
   static const String editAccount = '/edit_account';
   static const String attendance = '/attendance';
   static const String home = '/home';
   static const String notification = '/notification';
+  static const String profilePage = '/profile_page';
 
   static Map<String, WidgetBuilder> routes = {
-    editAccount: (context) => const SettingPage(),
+    profilePage: (context) => const ProfilePage(),
     attendance: (context) => const Attendance(),
     home: (context) => const HomePage(),
     notification: (context) => const NotificationScreen(),
